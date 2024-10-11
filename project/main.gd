9b9bb0056fef1d47068537d20cc18350478a7bfd
Lopes Manon <manon.lopes@etu.umontpellier.fr>
2024-10-11 16:14:42 +0200

    Ajout de nouveaux sprites

--- project/main.gd
+++ project/main.gd
@@ -45,10 +45,11 @@ func _on_mob_timer_timeout():
 
 	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
+	
 
 	# Add some randomness to the direction.
 	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+	#mob.rotation = direction
 
 	# Choose the velocity for the mob.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
